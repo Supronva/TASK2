3071cf4a2be2afa29fd2e39f07757cb95b41185a
Supronva <oag4400@gmail.com>
2015-04-20 23:55:59 +0300

    Task2

---
+++ epam1-2/Item.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace epam1_2
+{
+    class Item
+    {
+        private string nameGoods;// поле наименования товара
+        private int priceOneGoods;// поле стоимости 1й- еденицы товара
+        private int countGoods;//поле колв-о товара
+
+
+        /// <summary>
+        /// конструктор по умолчанию
+        /// </summary>
+        public Item()
+        {
+
+        }
+
+        /// <summary>
+        /// конструктор
+        /// </summary>
+        /// <param name="nameGoods">наименование товара</param>
+        /// <param name="priceOneGoods">стоимость 1-й еденицы товара</param>
+        /// <param name="countGoods">кол-во товара</param>
+        public Item(string nameGoods, int priceOneGoods, int countGoods)
+        {
+            this.nameGoods = nameGoods;
+            this.priceOneGoods = priceOneGoods;
+            this.countGoods = countGoods;
+        }
+        /// <summary>
+        /// свой-во наименования товара
+        /// </summary>
+        public string NameGoods
+        {
+            get { return nameGoods; }
+            set { nameGoods = value; }
+        }
+
+
+        /// <summary>
+        /// свой-во стоимости 1-й еденицы товара
+        /// </summary>
+        public int PriceOneGoods
+        {
+            get { return priceOneGoods; }
+            set { priceOneGoods = value; }
+        }
+
+
+        /// <summary>
+        /// свой-во коли-во товара
+        /// </summary>
+        public int CountGoods
+        {
+            get { return countGoods; }
+            set { countGoods = value; }
+        }
+
+        /// <summary>
+        /// метод возвращающий общую стоимость товара
+        /// </summary>
+        /// <returns></returns>
+        public int GetTotalprice()
+        {
+            return priceOneGoods * countGoods;
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format("Наименование товара = {0}\nСтоимость 1-й еденицы товара = {1}\nКол-во товара = {2}", nameGoods, priceOneGoods, countGoods);
+        }
+
+
+
+        public static ArrayList NewArrayCollection(int i)
+        {
+            Random r = new Random();
+            ArrayList array = new ArrayList();
+
+            for (int j = 0; j < i; j++)
+
+                array.Add(r.Next(1, 10));
+                return array;
+            
+        }
+
+
+
+        public static void WriteMyNewArrayCollection(ArrayList array)
+        {
+            foreach (int a in array)
+                Console.Write("{0}\t", a);
+            Console.WriteLine("\n");
+        }
+
+    }
+}
---
+++ epam1-2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace epam1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 2");
+            Console.WriteLine(new string('-', 50));
+            Item iname = new Item("Телефон Samsung",100,5);
+            Console.WriteLine(iname.ToString());
+            Console.WriteLine("Общая стоимость товара = {0}",iname.GetTotalprice());
+
+            Console.WriteLine(new string('-', 50));
+
+            Item iname1 = new Item("Телефон HTC", 90, 5);
+            Console.WriteLine(iname1.ToString());
+            Console.WriteLine("Общая стоимость товара = {0}", iname1.GetTotalprice());
+
+            Console.WriteLine(new string('-', 50));
+
+            ArrayList arrayitem = new ArrayList();
+            arrayitem.Add(iname.GetTotalprice());
+            arrayitem.Add(iname1.GetTotalprice());
+            int sum = 0;
+            foreach (int ai in arrayitem)
+            {
+                sum += ai;
+            }
+            Console.WriteLine("Общая стоимость товаров = {0}", sum);
+
+            Console.WriteLine(new string('-', 50));
+
+            int x=100;
+            ArrayList myarray = Item.NewArrayCollection(x);
+            Item.WriteMyNewArrayCollection(myarray);
+
+           Console.ReadKey();
+        }
+    }
+}
